2ebc2e4b246c1580b8b016332b3f39009863e075
amalon2022 <112629936+amalon2022@users.noreply.github.com>
2022-09-27 08:46:18 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿// Напишите программу, которая на вход
-// принимает число и выдаёт его квадрат (число
-// умноженное на само себя).
-// Например:
-// 4 -> 16
-// -3 -> 9
-// -7 -> 49
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Ведите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square =number * number;
-Console.WriteLine($"Квадрат числа {number} = {square}");
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int maximum = num1;
+if (num2 > maximum)maximum = num2;
+if (num3 > maximum)maximum = num3;
+{
+    Console.WriteLine("maximum = " + maximum.ToString());
+}
