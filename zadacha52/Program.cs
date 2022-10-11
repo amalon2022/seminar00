4b30c25924759c683c571990646fc0fd80911252
amalon2022 <112629936+amalon2022@users.noreply.github.com>
2022-10-11 09:45:54 +0600

    Add files via upload

---
+++ zadacha52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+        // Например, задан массив:
+        // 1 4 7 2
+        // 5 9 2 3
+        // 8 4 2 4
+        // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+        
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        Console.Write("Введите m: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"m = {m}, n = {n}.");
+
+
+
+        int[,] array = new int[m, n];
+
+
+        CreateArrayInt(array);
+        WriteArrayInt(array);
+
+        Console.Write($"\nCреднее арифметическое:\n");
+        for (int i = 0; i < n; i++)
+        {
+            double arithmeticMean = 0;
+            for (int j = 0; j < m; j++)
+            {
+                arithmeticMean += array[j, i];
+            }
+            arithmeticMean = Math.Round(arithmeticMean / m, 1);
+            Console.WriteLine($"столбца № {i + 1} {arithmeticMean}");
+        }
+
+        void CreateArrayInt(int[,] array)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = new Random().Next(10);
+                }
+            }
+        }
+
+        void WriteArrayInt(int[,] array)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
