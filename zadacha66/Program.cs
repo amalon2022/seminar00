a5b96b604ac49533d68b25006421ab6b40be75f2
amalon2022 <112629936+amalon2022@users.noreply.github.com>
2022-10-17 23:50:54 +0600

    Add files via upload

---
+++ zadacha66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите первое число ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+sum += NaturalSumFromMToN(numberM, numberN);
+Console.Write($"Сумма = {sum} ");
+
+int NaturalSumFromMToN(int numM, int numN)
+{
+    if (numN == numM) return numM;
+    if (numM < numN)
+    {
+
+        return numM + NaturalSumFromMToN(numM + 1, numN);
+    }
+    if (numM > numN)
+    {
+        return numM + NaturalSumFromMToN(numM - 1, numN);
+    }
+    return 0;
+}
